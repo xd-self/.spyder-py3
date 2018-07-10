937f4e537d6f818b487b8894b71511e93be75ada
Young <wxyz1902@163.com>
2018-07-10 18:15:08 +0800

    amend

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ from pandas import Series, DataFrame
 import pandas as pd
 import matplotlib.pyplot as plt
 
-s1 = Series(np.arange(15))
+
 '''fig = plt.figure()
 ax1 = fig.add_subplot(1,3,1)
 ax2 = fig.add_subplot(1,3,2)
@@ -12,10 +12,11 @@ ax3 = fig.add_subplot(1,3,3)
 
 ax1.plot(s1)'''
 s2 = np.random.randn(10)
+s1 = Series(np.arange(15))
 fig, axe = plt.subplots(2,3,sharex=True,sharey=True)
 
 
 
 fig.subplots_adjust(wspace=0,hspace=0)# not effective?
 
-
+[ hex(x).replace('0x','') for x in range(256)]
